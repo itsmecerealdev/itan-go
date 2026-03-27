1d72facb0fcfc35dcfb4b247af0dd3320c3da6dd
~noodlez1232 <contact@nathanielbarragan.xyz>
2026-03-26 17:25:10 -0700

    token: tokenization [part 1]
    The easy bits include:
    - TokenType
    - Single character tokens
    - Skipping useless stuff
    - Erroring on any invalid characters
    - Half of identifier parsing

--- cmd/compiler.go
+++ cmd/compiler.go
@@ -6,5 +6,13 @@ import(
 )
 
 func main() {
-	fmt.Println(token.Test2())
+	var buffer string = "  hello ^"
+	tokens, err := token.Tokenize(buffer)
+	if (err != nil) {
+		fmt.Printf("Error: %s\n", err);
+		return
+	}
+	for _, t := range tokens {
+		fmt.Printf("Token: %s, Name: %s\n", t.Type, t.Name)
+	}
 }
--- lib/token/token.go
+++ lib/token/token.go
@@ -1,5 +1,145 @@
 package token
 
-func Test() string {
-	return "hello"
+import (
+	"errors"
+	"unicode"
+	"fmt"
+	"slices"
+)
+
+
+type TokenType int
+
+const (
+	Number TokenType = iota
+	Plus
+	Minus
+	Star
+	Slash
+	Exponent
+	LParen
+	RParen
+	Comma
+	Assignment
+	Not
+	Equal
+	NotEqual
+	LessThan
+	GreaterThan
+	LessEqual
+	GreaterEqual
+	And
+	Or
+	Type
+	StatementEnd
+	Identifier
+	FuncIdentifier
+	Return
+	LBrace
+	RBrace
+	End
+)
+
+var tokenName = map[TokenType]string {
+	Number: "Number",
+	Plus: "Plus",
+	Minus: "Minus",
+	Star: "Star",
+	Slash: "Slash",
+	Exponent: "Exponent",
+	LParen: "LParen",
+	RParen: "RParen",
+	Comma: "Comma",
+	Assignment: "Assignment",
+	Not: "Not",
+	Equal: "Equal",
+	NotEqual: "NotEqual",
+	LessThan: "LessThan",
+	GreaterThan: "GreaterThan",
+	LessEqual: "LessEqual",
+	GreaterEqual: "GreaterEqual",
+	And: "And",
+	Or: "Or",
+	Type: "Type",
+	StatementEnd: "StatementEnd",
+	Identifier: "Identifier",
+	FuncIdentifier: "FuncIdentifier",
+	Return: "Return",
+	LBrace: "LBrace",
+	RBrace: "RBrace",
+	End: "End",
+}
+
+func (tt TokenType)String() string {
+	return tokenName[tt]
+}
+
+
+type Token struct {
+	Type TokenType
+	Name string
+}
+
+var symbolToTokenType = map[rune]TokenType {
+	'+': Plus,
+	'-': Minus,
+	'*': Star,
+	'/': Slash,
+	'^': Exponent,
+	'(': LParen,
+	')': RParen,
+	',': Comma,
+	'=': Assignment,
+	'!': Not,
+	'<': LessThan,
+	'>': GreaterThan,
+	'&': And,
+	'|': Or,
+	';': StatementEnd,
+	'{': LBrace,
+	'}': RBrace,
+}
+
+func Tokenize(buf string) ([]Token, error) {
+	if len(buf) == 0 {
+		return nil, errors.New("Buffer is empty!")
+	}
+
+	var buffer []rune = []rune(buf)
+	var return_tokens []Token
+	var buffer_index int = 0
+	for buffer_index < len(buffer) {
+		fmt.Println(buffer_index)
+		var c = buffer[buffer_index]
+		if unicode.IsSpace(c) {
+			buffer_index++
+			continue
+		}
+		if (!unicode.IsLetter(c) && !isSymbol(c)) {
+			return nil, errors.New("Invalid character")
+		}
+
+		if isSymbol(c) {
+			return_tokens = append(return_tokens, Token{symbolToTokenType[c], ""})
+			buffer_index++
+			continue
+		}
+
+		if (unicode.IsLetter(c)) {
+			var start_index = buffer_index
+			var c = buffer[buffer_index];
+			for (buffer_index < len(buffer)) && unicode.IsSpace(c) && !isSymbol(c) {
+				buffer_index ++;
+			}
+			var str = buffer[start_index:buffer_index]
+		}
+	}
+
+	return_tokens = append(return_tokens, Token{End, "end"})
+	return return_tokens, nil
+}
+
+func isSymbol(c rune) bool {
+	var valid_symbols []rune = []rune("()+-*/=;^,{}<>!")
+	return slices.Contains(valid_symbols, c)
 }
